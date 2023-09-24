1b8cc5b9101e5d5a5bb08314f939e40e9e263d0d
sohan004 <aryansohan02@gmail.com>
2023-09-24 14:44:12 +0600

    cng

--- src/components/Home/Section/NewSection1.jsx
+++ src/components/Home/Section/NewSection1.jsx
@@ -7,7 +7,7 @@ const NewSection1 = () => {
     return (
         <div className="home bg-white">
             <div className="max-w-[1440px] mx-auto px-4 pt-12">
-                <h1 className="text-center text-2xl lg:text-4xl lg:leading-[64px] leading-[44px] mb-8 lg:mb-16 font-extrabold text-blue-950">Join Thousands of Australians
+                <h1 className="text-center text-2xl lg:text-4xl lg:leading-[64px] leading-[44px] mb-8 lg:mb-16 font-extrabold text-blue-950">Join Thousands of Australians <br />
                     Lease Your Spare Room & Find Your Perfect Room</h1>
                 <NewSection2 />
             </div>
